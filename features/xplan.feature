23213ce9c1b7aae0d8cf9d201b270452aa294107
GRXCYQ <cory_greatrex@amp.com.au>
2019-05-20 16:38:47 +1000

    Finished basic POM

--- features/xplan.feature
+++ features/xplan.feature
@@ -1,10 +1,5 @@
 Feature: Test xplan login page
-  Scenario: Verify the title of the login page
+  Scenario: Login to xplan
     Given I am on the charter login page
-    When I get the title of the page
-    Then the title is "XPLAN Today | AMP XPLAN"
-  
-#   Scenario: Login to xplan
-#     Given I am on the charter login page
-#     When I login to xplan charter
-#     Then the title is "XPLAN Today | AMP XPLAN"
+    When I login to xplan charter
+    Then the title is not "XPLAN Today | AMP XPLAN"
--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,20 @@
+var path = require('path');
+var VisualRegressionCompare = require('wdio-visual-regression-service/compare');
+ 
+function getScreenshotName(basePath) {
+  return function(context) {
+    var type = context.type;
+    var testName = context.step.name;
+    var browserVersion = parseInt(context.browser.version, 10);
+    var browserName = context.browser.name;
+    var browserViewport = context.meta.viewport;
+    var browserWidth = browserViewport.width;
+    var browserHeight = browserViewport.height;
+ 
+    return path.join(basePath, `${testName}_${type}_${browserName}_v${browserVersion}_${browserWidth}x${browserHeight}.png`);
+  };
+}
+
 exports.config = {
     
     // Override the default path of /wd/hub
@@ -109,7 +126,19 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver'],
+    services: ['chromedriver', 'visual-regression'],
+
+    visualRegression: {
+      compare: new VisualRegressionCompare.LocalCompare({
+        referenceName: getScreenshotName(path.join(process.cwd(), 'screenshots/baseline')),
+        screenshotName: getScreenshotName(path.join(process.cwd(), 'screenshots/actual')),
+        diffName: getScreenshotName(path.join(process.cwd(), 'screenshots/diff')),
+        misMatchTolerance: 0.01,
+      }),
+      viewportChangePause: 300,
+      viewports: [{ width: 1024, height: 768 }],
+      orientations: ['landscape'],
+    },
 
     //chromeDriverArgs: ['--port=9515', '--url-base=\'/\''], // default for ChromeDriver
     //chromeDriverLogs: './',
@@ -136,7 +165,7 @@ exports.config = {
     //     timeout: 60000
     // },
     cucumberOpts: {
-        require: ['./test/specs/step-def.js'],   // <string[]> (file/dir) require files before executing features
+        require: ['./step_definitions/*.js'],   // <string[]> (file/dir) require files before executing features
         backtrace: true,    // <boolean> show full backtrace for errors
         compiler: ['js:babel-core/register'], // <string[]> filetype:compiler used for processing required features
         failAmbiguousDefinitions: false,       // <boolean< Treat ambiguous definitions as errors
